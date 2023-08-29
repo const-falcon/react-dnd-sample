f0db4d40b521cd8d1ae070ae34df06444951413b
g-auctioneer <93477932+g-auctioneer@users.noreply.github.com>
2023-08-29 23:54:35 +0900

    🚧 wip...

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,7 @@
+import { useRef, useState } from "react";
 import { useDrag, useDrop } from "react-dnd";
 import type { XYCoord } from "react-dnd";
 import "./Board.css";
-import { useState } from "react";
 
 type Position = {
   top: number;
@@ -10,6 +10,23 @@ type Position = {
 
 const Board = () => {
   const [position, setPosition] = useState<Position>({ top: 0, left: 0 });
+  const boardRef = useRef<HTMLDivElement>(null);
+
+  const isOnBoard = (delta: XYCoord) => {
+    // 上端を判定
+    if (position.top + delta.y < 0) return false;
+
+    // 右端を判定
+    // 100pxベタ書きはせず、要素を参照して幅を取りたい
+
+    // 左端を判定
+    if (position.left + delta.x < 0) return false;
+
+    // 下端を判定
+    // 100pxベタ書きはせず、要素を参照して幅を取りたい
+
+    return true;
+  };
 
   const [{ isDragging }, drag] = useDrag(
     {
@@ -22,11 +39,12 @@ const Board = () => {
     [position]
   );
 
-  const [_, drop] = useDrop(
+  const [{ canDrop }, drop] = useDrop(
     () => ({
       accept: "square",
-      drop(_, monitor) {
-        const delta = monitor.getDifferenceFromInitialOffset() as XYCoord;
+      drop: (_, monitor) => {
+        const delta = monitor.getDifferenceFromInitialOffset() as XYCoord; // yは上に動くと-になる...
+        // console.log(delta);
         setPosition((prev) => {
           return {
             top: Math.round(prev.top + delta.y),
@@ -34,23 +52,34 @@ const Board = () => {
           };
         });
       },
+      canDrop: (_, monitor) => {
+        // ボード外にdropできないようにしたい
+        const delta = monitor.getDifferenceFromInitialOffset() as XYCoord;
+        // console.log(delta);
+        return isOnBoard(delta);
+      },
+      collect: (monitor) => ({
+        canDrop: !!monitor.canDrop(),
+      }),
     }),
     []
   );
 
   return (
-    <div id="board" ref={drop}>
-      <p
-        className="square"
-        ref={drag}
-        style={{
-          ...position,
-          display: isDragging ? "none" : "block",
-        }}
-      >
-        動かせるよ
-      </p>
-      <p className="background-text">dropできるよーん</p>
+    <div id="board" ref={boardRef}>
+      <div ref={drop}>
+        <p
+          className="square"
+          ref={drag}
+          style={{
+            ...position,
+            display: isDragging ? "none" : "block",
+          }}
+        >
+          動かせるよ
+        </p>
+        <p className="background-text">dropできるよーん</p>
+      </div>
     </div>
   );
 };
